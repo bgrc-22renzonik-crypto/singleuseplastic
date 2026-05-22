f81987b648c8a402bf4ba45628249048274945f6
bgrc-22renzonik-crypto <bgrc-22renzonik@bishopgore.net>
2026-05-22 22:35:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
@@ -173,5 +172,4 @@ function showFact(){
 """
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=0)
